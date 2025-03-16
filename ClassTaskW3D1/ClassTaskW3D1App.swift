114326dc86e8d00c30ecd886398273122cd3970a
Rawann0m <raawannm@gmail.com>
2025-03-16 11:23:47 +0300

    Initial Commit

---
+++ ClassTaskW3D1/ClassTaskW3D1App.swift
@@ -0,0 +1,17 @@
+//
+//  ClassTaskW3D1App.swift
+//  ClassTaskW3D1
+//
+//  Created by Rawan on 16/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ClassTaskW3D1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
